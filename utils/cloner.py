d627a8c49c35fd06b627bf7cf5a9fe85b76817f0
raptor1820 <57589076+raptor1820@users.noreply.github.com>
2023-04-05 17:55:10 +0530

    added openai

---
+++ utils/cloner.py
@@ -0,0 +1,1 @@
+#11Labs cloning stuff here
---
+++ utils/speech_to_text_and_response.py
@@ -0,0 +1,9 @@
+#parse audio input and use gpt to generate response
+import openai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+openai.api_key = os.getenv('OPENAI_API_KEY')
+print(openai.api_key)
