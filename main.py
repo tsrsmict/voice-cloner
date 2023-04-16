fc23244df84c09024e24e6712a24f673218d0fa1
Arjun Sharma <arjun.sharma07@outlook.com>
2023-04-16 19:26:12 +0530

    Add basic flask app

--- main.py
+++ main.py
@@ -1,41 +1,37 @@
 import json
-from typing import Union, Optional
+from typing import Optional
 
-from tkinter import Tk, StringVar, OptionMenu, Button, Label
+from flask import Flask, render_template
+
 
 from cloner import get_audio, VoiceClone, CloneConversation
+
 
 voices_data = json.loads(open("voices.json").read())["voices"]
 voices = [VoiceClone(voice_data) for voice_data in voices_data]
 
-root = Tk()
-Label(root, text="Talk to a celebrity!").pack()
-
-clicked = StringVar()
-options = [voice.name for voice in voices]
-clicked.set(options[0])
-voices_select_dropdown = OptionMenu(root, clicked, *options)
-voices_select_dropdown.pack()
-
 conversation: Optional[CloneConversation] = None
 
-def start_conversation():
-    voices_select_dropdown.pack_forget()
-    voice = voices[options.index(clicked.get())]
+def start_conversation(voice: VoiceClone = voices[0]):
     conversation = CloneConversation(voice)
 
     num_inputs = 0
-
-    while num_inputs < 6:
+    while num_inputs < 20:
         user_message = get_audio()
         conversation.play_response_to_new_message(user_message)
         num_inputs += 1
 
-button = Button(root, text="Start talking!", command=start_conversation).pack()
+app = Flask(__name__)
 
-def end_conversation():
-    ...
+@app.route("/")
+def index():
+    return render_template("index.html")
 
+@app.route("/start-conversation")
+def start_conversation_route():
+    start_conversation()
+    return "Started conversation"
 
-# Execute tkinter
-root.mainloop()
+if __name__=="__main__":
+    app.run(debug=True)
+
